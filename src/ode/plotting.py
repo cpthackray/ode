f7cbf177f6ddbaa3d6b426f10f50855ceff222e0
Colin Thackray <thackray@colins-mac-mini.home>
2025-03-12 17:00:00 -0230

    packaging

---
+++ src/ode/plotting.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Plots
+
+
+# state space plot grid; color by time
+
+# time series plot for (choice of) state variables
